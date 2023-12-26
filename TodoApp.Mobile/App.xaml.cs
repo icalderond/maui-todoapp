73d10a5853024a3ab3e6d24018ca87a42ab44af7
Israel Calderon de la Cruz <icalderond@outlook.com>
2023-12-25 21:00:38 -0600

    First commit

---
+++ TodoApp.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ TodoApp.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
