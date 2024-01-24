3c267e053b66e3249acdf78ca0386fe16799621b
Israel Calderon de la Cruz <icalderond@outlook.com>
2024-01-24 08:01:18 -0600

    Update UI behavior

--- TodoApp.Mobile/ViewModels/CreateNewTodoItemViewModel.cs
+++ TodoApp.Mobile/ViewModels/CreateNewTodoItemViewModel.cs
@@ -1,10 +1,41 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TodoApp.Mobile.Interfaces;
+using TodoApp.Shared.Models;
 
 namespace TodoApp.Mobile.ViewModels;
 
 public class CreateNewTodoItemViewModel : ObservableObject
 {
-    public CreateNewTodoItemViewModel()
+    #region Private Properties
+    private readonly ITodoItemService _todoItemService;
+    #endregion Private Properties
+
+    #region Lifecycle Methods
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public CreateNewTodoItemViewModel(ITodoItemService todoItemService)
     {
+        _todoItemService = todoItemService;
+        _ = Initialize();
     }
+    #endregion Lifecycle Methods
+    
+    #region Public Methods
+    /// <summary>
+    /// Load initial data
+    /// </summary>
+    private async Task Initialize()
+    {
+        
+    }
+
+    [RelayCommand]
+    private async Task OpenNote(object arg)
+    {
+        _todoItemService
+    }
+    #endregion Public Methods
 }
