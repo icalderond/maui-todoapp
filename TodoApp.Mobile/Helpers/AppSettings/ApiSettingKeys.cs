c47695279b76848b28e75d8aae140e685d89b888
Israel Calderon de la Cruz <icalderond@outlook.com>
2024-01-29 00:01:48 -0600

    + randomuser uri

--- TodoApp.Mobile/Helpers/AppSettings/ApiSettingKeys.cs
+++ TodoApp.Mobile/Helpers/AppSettings/ApiSettingKeys.cs
@@ -4,4 +4,5 @@ public class ApiSettingKeys
 {
     public const string BaseHost = "http://192.168.1.102:5049/";
     public const string CRUDName = "todoitems";
+    public const string RandomUserUri = "https://randomuser.me/api/";
 }
