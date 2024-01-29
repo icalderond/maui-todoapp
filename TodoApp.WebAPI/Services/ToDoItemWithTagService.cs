e179e193f760443fddbd30cb946293fabc01c48f
Israel Calderon de la Cruz <icalderond@outlook.com>
2024-01-28 23:29:32 -0600

    Fix insert todoItem

--- TodoApp.WebAPI/Services/ToDoItemWithTagService.cs
+++ TodoApp.WebAPI/Services/ToDoItemWithTagService.cs
@@ -17,46 +17,51 @@ public class ToDoItemWithTagService
     {
         try
         {
-            List<Tag> tagsFromDb = new List<Tag>();
-            List<Tag> tagsToDb = new List<Tag>();
-
-            var tagsToInclude = todoItemRequest.TagRequests.Select(
-                x => new Tag
-                {
-                    Id = x.Id,
-                    Title = x.Title,
-                    Description = x.Description
-                }).ToList();
-
-            foreach (var tagRequest in tagsToInclude)
-            {
-                var tag = await _UnitOfWork._TagRepository.GetByTitle(tagRequest.Title);
-                if (tag != null)
-                {
-                    tagsFromDb.Add(tag);
-                }
-                else
-                {
-                    tagRequest.Id = default;
-                    tagsToDb.Add(tagRequest);
-                }
-            }
-
             var todoItem = new TodoItem()
             {
                 Title = todoItemRequest.Title,
                 Content = todoItemRequest.Content,
-                UpdateDate = DateTime.Now,
-                Tags = tagsToInclude
+                UpdateDate = DateTime.Now
             };
 
-            if (tagsToDb?.Any() == true)
-                tagsToDb
-                    .ForEach(x => x.ToDoItems = new List<TodoItem>() { todoItem });
+            var listOfTagRequests = todoItemRequest.TagRequests.ToList();
+            var tagsForTodo = new List<Tag>();
+            var tagsToInsert = new List<Tag>();
 
+            do
+            {
+                tagsToInsert = new List<Tag>();
+                foreach (var tagRequest in listOfTagRequests)
+                {
+                    var tag = await _UnitOfWork._TagRepository.GetByTitle(tagRequest.Title);
+                    if (tag != null)
+                    {
+                        tagsForTodo.Add(tag);
+                    }
+                    else
+                    {
+                        tagsToInsert.Add(new Tag()
+                        {
+                            Title = tagRequest.Title,
+                            Description = tagRequest.Description
+                        });
+                    }
+                }
+
+                if (tagsToInsert.Any())
+                {
+                    await _UnitOfWork._TagRepository.Insert(tagsToInsert);
+                    await _UnitOfWork.Save();
+                    listOfTagRequests = listOfTagRequests.Where(x => tagsForTodo.All(y => y.Title != x.Title)).ToList();
+                }
+            } while (tagsToInsert.Any());
+
+
+            if (tagsForTodo?.Any() == true)
+                tagsForTodo 
+                    .ForEach(x => x.ToDoItems = new List<TodoItem>() { todoItem });
+            
             await _UnitOfWork._ToDoItemRepository.Insert(todoItem);
-            if (tagsToDb?.Any() == true)
-                await _UnitOfWork._TagRepository.Insert(tagsToDb);
             await _UnitOfWork.Save();
         }
         catch (Exception e)
