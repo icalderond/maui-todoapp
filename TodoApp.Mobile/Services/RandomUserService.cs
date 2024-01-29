bfebe36ef98f8fa2d44d6554a852bdae41ee9c9c
Israel Calderon de la Cruz <icalderond@outlook.com>
2024-01-29 00:01:21 -0600

    Fix RandomUserService.cs

--- TodoApp.Mobile/Services/RandomUserService.cs
+++ TodoApp.Mobile/Services/RandomUserService.cs
@@ -5,13 +5,15 @@ using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using TodoApp.Mobile.Helpers.AppSettings;
+using TodoApp.Mobile.Helpers.Services;
 using TodoApp.Mobile.Interfaces;
 using TodoApp.Mobile.Model;
 using TodoApp.Shared.Models;
 
 namespace TodoApp.Mobile.Services;
 
-public class RandomUserService : IRandomUserService
+public class RandomUserService : HttpClientService, IRandomUserService
 {
     private readonly string _randomUserUrl = "https://randomuser.me/api/";
 
@@ -20,13 +22,10 @@ public class RandomUserService : IRandomUserService
         RandomUser? randomUser = null;
         try
         {
-            HttpClient httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(new Uri(_randomUserUrl));
+            var response = await CallGetAsync<RandomUserResult>(ApiSettingKeys.RandomUserUri);
             if (response.StatusCode == HttpStatusCode.OK)
             {
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                RandomUserResult randomUserResult = JsonSerializer.Deserialize<RandomUserResult>(jsonResponse);
-                randomUser = randomUserResult?.Results.FirstOrDefault();
+                randomUser = response.Content?.Results.FirstOrDefault();
             }
         }
         catch (Exception e)
