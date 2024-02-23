faa3d2e0976d0b15af2d36dc2977cc99f0c2c190
Israel Calderon de la Cruz <icalderond@outlook.com>
2024-02-23 08:55:24 -0600

    Update server name from IIS server

--- TodoApp.Mobile/Helpers/AppSettings/ApiSettingKeys.cs
+++ TodoApp.Mobile/Helpers/AppSettings/ApiSettingKeys.cs
@@ -2,7 +2,7 @@ namespace TodoApp.Mobile.Helpers.AppSettings;
 
 public class ApiSettingKeys
 {
-    public const string BaseHost = "http://192.168.0.114:5049/";
+    public const string BaseHost = "http://192.168.1.114:17408/";
     public const string CRUDName = "todoitems";
     public const string RandomUserUri = "https://randomuser.me/api/";
 }
